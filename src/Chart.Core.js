2020366b0e3686895ce3d2818a473e29f91b8348
ulmeyda <shinya.sugar@gmail.com>
2015-03-12 20:44:23 +0900

    幅と高さをoffsetで持ってこないようにしたパターン

--- src/Chart.Core.js
+++ src/Chart.Core.js
@@ -24,21 +24,8 @@
 
 		this.ctx = context;
 
-		//Variables global to the chart
-		var computeDimension = function(element,dimension)
-		{
-			if (element['offset'+dimension])
-			{
-				return element['offset'+dimension];
-			}
-			else
-			{
-				return document.defaultView.getComputedStyle(element).getPropertyValue(dimension);
-			}
-		}
-
-		var width = this.width = computeDimension(context.canvas,'Width');
-		var height = this.height = computeDimension(context.canvas,'Height');
+		var width = this.width = context.canvas.width;
+		var height = this.height = context.canvas.height;
 
 		// Firefox requires this to work correctly
 		context.canvas.width  = width;
